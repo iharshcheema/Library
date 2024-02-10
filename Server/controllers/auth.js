c3af8bfae15f5ee0ecf352f44ff52b2500199a4f
Vansh=Narang <vanshnarang49906@gmail.com>
2024-02-10 21:06:41 +0530

    Hashing password done

--- Server/controllers/auth.js
+++ Server/controllers/auth.js
@@ -1,5 +1,5 @@
 const User = require("../models/User");
-
+const bcrypt = require("bcryptjs");
 
 exports.createUser = async (req, res) => {
     try {
@@ -11,10 +11,13 @@ exports.createUser = async (req, res) => {
                 error: "User already exists"
             })
         }
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
 
-        await User.create({ username, password, userId })
-
+        const newUser = new User({ username, password: hashedPassword, userId })
+        const user = await newUser.save();
         res.status(200).json({
+            user,
             message: "User created successfully"
         })
     } catch (error) {
